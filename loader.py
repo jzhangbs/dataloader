fa7c2e485bd7bc52b4c0225ef4eec0becb2d80c8
jzhangbs <zhangjingyang33@126.com>
2018-06-14 14:05:02 +0800

    support sceneflow

--- loader.py
+++ loader.py
@@ -1,53 +1,164 @@
 import multiprocessing as mp
-import sys, os
-import traceback as tb
+import multiprocessing.managers as managers
+import random
+import numpy as np
+import atexit
+import time
+import argparse
+import os
+import signal
+
+from process import gen_sample_list, split_sample_list, read
+
+
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
+
+
+def get_batch(self, batch_size, phase):
+    outputs = []
+    for i in range(batch_size):
+        outputs.append(self.output_queue[phase].get())
+    output_np = []
+    for image_list in zip(*outputs):
+        output_np.append(np.stack(image_list, axis=0))
+    return output_np
 
 
 class Loader:
-    def __init__(self):
-        self.num_worker = 8  # TODO
-        self.queue_capacity = 50
+    def __init__(self, data_root, dataset_name, val_ratio, preproc_args, sys_args, server=False):
+        self.subproc = mp.Queue()
+        atexit.register(self.kill_children)
 
-        self.filename_queue = mp.Queue(self.queue_capacity)
-        self.output_queue = mp.Queue(self.queue_capacity)
+        self.num_worker = sys_args.num_worker
+        self.queue_capacity = sys_args.queue_capacity
+        if server: self.port = sys_args.port
+        if server: self.auth = sys_args.auth
 
-        self.main_process = mp.Process(target=self.main, name='loader')
+        self.data_root = data_root
+        self.dataset_name = dataset_name
+        self.val_ratio = val_ratio
+        self.preproc_args = preproc_args
 
-    def __enter__(self):
-        self.start()
-        return self
+        filename_list = gen_sample_list(self.data_root, self.dataset_name)
+        training_list, validation_list = split_sample_list(filename_list, self.val_ratio, seed=0)
+        self.training_sample_size = len(training_list)
+        self.validation_sample_size = len(validation_list)
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.stop()
-        tb.print_exception(exc_type, exc_val, exc_tb)
+        self.filename_queue = {}
+        self.output_queue = {}
+        self.filename_queue['train'] = mp.Queue(self.queue_capacity)
+        self.output_queue['train'] = mp.Queue(self.queue_capacity)
+        self.train_process = mp.Process(target=self.main, name='train_loader', kwargs={
+            'filename_list': training_list,
+            'phase': 'train'
+        })
+        self.filename_queue['val'] = mp.Queue(self.queue_capacity)
+        self.output_queue['val'] = mp.Queue(self.queue_capacity)
+        self.val_process = mp.Process(target=self.main, name='val_loader', kwargs={
+            'filename_list': validation_list,
+            'phase': 'val'
+        })
+
+        if server:
+            managers.SyncManager.register('global_namespace', callable=lambda: {
+                'training_sample_size': self.training_sample_size,
+                'validation_sample_size': self.validation_sample_size
+            })
+            managers.SyncManager.register('train_output_queue', callable=lambda: self.output_queue['train'])
+            managers.SyncManager.register('val_output_queue', callable=lambda: self.output_queue['val'])
+            self.manager = managers.SyncManager(('', self.port), self.auth)
+            self.manager.start()
+
+    def kill_children(self):
+        while not self.subproc.empty():
+            pid = self.subproc.get_nowait()
+            os.kill(pid, signal.SIGTERM)
 
     def start(self):
-        self.main_process.start()
+        self.train_process.start()
+        self.subproc.put(self.train_process.pid)
+        self.val_process.start()
+        self.subproc.put(self.val_process.pid)
+        return self
 
     def stop(self):
-        self.main_process.terminate()
+        pass
 
-    def main(self):
+    def main(self, filename_list, phase):
+        self.phase = phase
+
         process = []
         for i in range(self.num_worker):
-            p = mp.Process(target=self.worker, name='worker-%d' % i, kwargs={}, daemon=True)  # TODO
+            p = mp.Process(target=self.worker, name=phase+'_worker-%d' % i, kwargs={})
             process.append(p)
             p.start()
-
-        filename_list = []  # TODO
+            self.subproc.put(p.pid)
 
         while True:
+            if phase == 'train':
+                random.shuffle(filename_list)
             for filename in filename_list:
-                self.filename_queue.put(filename)
+                self.filename_queue[phase].put(filename)
 
     def worker(self):
         while True:
-            filename = self.filename_queue.get()
+            filename = self.filename_queue[self.phase].get()
+            images = read(filename, dotdict(
+                dataset_name=self.dataset_name,
+                phase=self.phase,
+                preproc_args=self.preproc_args
+            ))
+            self.output_queue[self.phase].put(images)
 
-            output = None  # TODO
+    def get_batch(self, batch_size, phase):
+        return get_batch(self, batch_size, phase)
 
-            self.output_queue.put(output)
 
-    def get_batch(self, batch_size, aug=True):
-        for i in range(batch_size):
-            output = self.output_queue.get()
+class LoaderClient:
+
+    def __init__(self, addr, port, auth):
+        self.addr = addr
+        self.port = port
+        self.auth = auth
+
+        managers.SyncManager.register('train_output_queue')
+        managers.SyncManager.register('val_output_queue')
+        managers.SyncManager.register('global_namespace')
+        self.manager = managers.SyncManager((self.addr, self.port), self.auth)
+        self.manager.connect()
+        self.training_sample_size = self.manager.global_namespace().get('training_sample_size')
+        self.validation_sample_size = self.manager.global_namespace().get('validation_sample_size')
+        self.output_queue = {
+            'train': self.manager.train_output_queue(),
+            'val': self.manager.val_output_queue()
+        }
+
+    def get_batch(self, batch_size, phase):
+        return get_batch(self, batch_size, phase)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_root', type=str)
+    parser.add_argument('--dataset_name', type=str, default='monkaa,flyingthings3d,driving')
+    parser.add_argument('--val_ratio', type=float, default=0.1)
+
+    parser.add_argument('--crop_width', type=int, default=512)
+    parser.add_argument('--crop_height', type=int, default=256)
+
+    parser.add_argument('--num_worker', type=int, default=2)
+    parser.add_argument('--queue_capacity', type=int, default=10)
+    parser.add_argument('--port', type=int, default=50000)
+    parser.add_argument('--auth', type=str, default='auth')
+    args = parser.parse_args()
+
+    Loader(args.data_root, args.dataset_name.split(','), args.val_ratio,
+           dotdict(crop_width=args.crop_width, crop_height=args.crop_height),
+           dotdict(num_worker=args.num_worker, queue_capacity=args.queue_capacity, port=args.port, auth=args.auth),
+           server=True).start()
+    while True:
+        time.sleep(5)
---
+++ process.py
@@ -0,0 +1,233 @@
+import re
+import numpy as np
+import sys
+import os
+import glob
+import math
+import random
+from imageio import imread
+
+
+def load_pfm(file):
+    color = None
+    width = None
+    height = None
+    scale = None
+    endian = None
+    header = file.readline().rstrip()
+    if header == b'PF':
+        color = True
+    elif header == b'Pf':
+        color = False
+    else:
+        raise Exception('Not a PFM file.')
+    dim_match = re.match(br'^(\d+)\s(\d+)\s$', file.readline())
+    if dim_match:
+        width, height = map(int, dim_match.groups())
+    else:
+        raise Exception('Malformed PFM header.')
+    scale = float(file.readline().rstrip())
+    if scale < 0: # little-endian
+        endian = '<'
+        scale = -scale
+    else:
+        endian = '>' # big-endian
+    data = np.fromfile(file, endian + 'f')
+    shape = (height, width, 3) if color else (height, width)
+    data = np.reshape(data, shape)
+    data = data[::-1, ...]  # cv2.flip(data, 0)
+    return data
+
+
+def write_pfm(file, image, scale=1):
+    file = open(file, 'wb')
+    color = None
+
+    if image.dtype.name != 'float32':
+        raise Exception('Image dtype must be float32.')
+
+    image = np.flipud(image)
+
+    if len(image.shape) == 3 and image.shape[2] == 3: # color image
+        color = True
+    elif len(image.shape) == 2 or len(image.shape) == 3 and image.shape[2] == 1: # greyscale
+        color = False
+    else:
+        raise Exception('Image must have H x W x 3, H x W x 1 or H x W dimensions.')
+
+    file.write('PF\n' if color else 'Pf\n')
+    file.write('%d %d\n' % (image.shape[1], image.shape[0]))
+
+    endian = image.dtype.byteorder
+
+    if endian == '<' or endian == '=' and sys.byteorder == 'little':
+        scale = -scale
+
+    file.write('%f\n' % scale)
+
+    image.tofile(file)
+
+
+def gen_driving_path(root_data_folder, root_disp_folder):
+    left_folders = []
+    right_folders = []
+    disp_folders = []
+    level1 = ["15mm_focallength", "35mm_focallength"]
+    level2 = ["scene_backwards", "scene_forwards"]
+    level3 = ["fast", "slow"]
+    sides = ["left", "right"]
+    for l1 in level1:
+        for l2 in level2:
+            for l3 in level3:
+                left_folders.append(os.path.join(root_data_folder, *[l1, l2, l3, sides[0]]))
+                right_folders.append(os.path.join(root_data_folder, *[l1, l2, l3, sides[1]]))
+                disp_folders.append(os.path.join(root_disp_folder, *[l1, l2, l3, sides[0]]))
+
+    left_images = []
+    right_images = []
+    disp_images = []
+    for left_folder, right_folder, disp_folder in zip(left_folders, right_folders, disp_folders):
+        left_data = sorted(glob.glob(left_folder + "/*.png"))
+        right_data = sorted(glob.glob(right_folder + "/*.png"))
+        disps_data = sorted(glob.glob(disp_folder + "/*.pfm"))
+        left_images = left_images + left_data
+        right_images = right_images + right_data
+        disp_images = disp_images + disps_data
+    return list(zip(left_images, right_images, disp_images))
+
+
+def gen_monkaa_path(root_data_folder, root_disp_folder):
+    left_folders = []
+    right_folders = []
+    disp_folders = []
+    level1 = sorted(os.listdir(root_data_folder))
+    sides = ["left", "right"]
+    for l1 in level1:
+        left_folders.append(os.path.join(root_data_folder, *[l1, sides[0]]))
+        right_folders.append(os.path.join(root_data_folder, *[l1, sides[1]]))
+        disp_folders.append(os.path.join(root_disp_folder, *[l1, sides[0]]))
+
+    left_images = []
+    right_images = []
+    disp_images = []
+    for left_folder, right_folder, disp_folder in zip(left_folders, right_folders, disp_folders):
+        left_data = sorted(glob.glob(left_folder + "/*.png"))
+        right_data = sorted(glob.glob(right_folder + "/*.png"))
+        disps_data = sorted(glob.glob(disp_folder + "/*.pfm"))
+        left_images = left_images + left_data
+        right_images = right_images + right_data
+        disp_images = disp_images + disps_data
+    return list(zip(left_images, right_images, disp_images))
+
+
+def gen_flyingthings3d_path(root_data_folder, root_disp_folder):
+    left_folders = []
+    right_folders = []
+    disp_folders = []
+    level1 = ["TRAIN"]
+    level2 = ["A", "B", "C"]
+    sides = ["left", "right"]
+    for l1 in level1:
+        for l2 in level2:
+            data_level12 = os.path.join(root_data_folder, *[l1, l2])
+            disp_level12 = os.path.join(root_disp_folder, *[l1, l2])
+            level3 = sorted(os.listdir(data_level12))
+            for l3 in level3:
+                left_folders.append(os.path.join(data_level12, *[l3, sides[0]]))
+                right_folders.append(os.path.join(data_level12, *[l3, sides[1]]))
+                disp_folders.append(os.path.join(disp_level12, *[l3, sides[0]]))
+
+    left_images = []
+    right_images = []
+    disp_images = []
+    for left_folder, right_folder, disp_folder in zip(left_folders, right_folders, disp_folders):
+        left_data = sorted(glob.glob(left_folder + "/*.png"))
+        right_data = sorted(glob.glob(right_folder + "/*.png"))
+        disps_data = sorted(glob.glob(disp_folder + "/*.pfm"))
+        left_images = left_images + left_data
+        right_images = right_images + right_data
+        disp_images = disp_images + disps_data
+    return list(zip(left_images, right_images, disp_images))
+
+
+def gen_sample_list(data_root, dataset_name):
+    """output paths in a list: [[img_path1, image_path2, disp_image1], [], ...]"""
+    # driving
+    driving_data_folder = os.path.join(data_root, 'driving/frames_cleanpass')
+    driving_label_folder = os.path.join(data_root, 'driving/disparity')
+    # monkaa
+    monkaa_data_folder = os.path.join(data_root, 'monkaa/frames_cleanpass')
+    monkaa_label_folder = os.path.join(data_root, 'monkaa/disparity')
+    # flyingthings3d
+    flyingthings3d_data_folder = os.path.join(data_root, 'flyingthings3d/frames_cleanpass')
+    flyingthings3d_label_folder = os.path.join(data_root, 'flyingthings3d/disparity')
+    # generate the list of all [left, right, disp]
+    sample_list = []
+    if 'driving' in dataset_name:
+        driving_sample_list = gen_driving_path(driving_data_folder, driving_label_folder)
+        sample_list = sample_list + driving_sample_list
+    if 'monkaa' in dataset_name:
+        monkaa_sample_list = gen_monkaa_path(monkaa_data_folder, monkaa_label_folder)
+        sample_list = sample_list + monkaa_sample_list
+    if 'flyingthings3d' in dataset_name:
+        flyingthings3d_sample_list = gen_flyingthings3d_path(flyingthings3d_data_folder, flyingthings3d_label_folder)
+        sample_list = sample_list + flyingthings3d_sample_list
+    return sample_list
+
+
+def split_sample_list(sample_list, val_ratio, fraction=1, seed=None):
+    random.seed(seed)
+    random.shuffle(sample_list)
+    num_samples = len(sample_list)
+    num_data = int(fraction * num_samples)
+    num_validation = int(math.ceil(num_data * val_ratio))
+    sample_list = sample_list[0:num_data]
+    validation_list = sample_list[0:num_validation]
+    training_list = sample_list[num_validation:]
+    return training_list, validation_list
+
+
+def center_image(img):
+    """ normalize image """
+    img = img.astype(np.float32)
+    var = np.var(img, axis=(0,1), keepdims=True)
+    mean = np.mean(img, axis=(0,1), keepdims=True)
+    return (img - mean) / (np.sqrt(var) + 0.00000001)
+
+
+def read(filename, kwargs):
+    ldata, rdata, ddata = filename
+    # read images
+    left_image = center_image(imread(ldata))
+    right_image = center_image(imread(rdata))
+    if 'kitti' not in kwargs.dataset_name:
+        disp_image = load_pfm(open(ddata, 'rb'))
+    else:
+        disp_image = imread(ddata, mode='L')
+    disp_image = np.expand_dims(disp_image, 2)
+    if kwargs.phase == 'val':
+        # return stereo pair
+        h, w = left_image.shape[0:2]
+        crop_width = w - w % 16
+        crop_height = h - h % 16
+        start_w = random.randint(0, w - crop_width)
+        start_h = random.randint(0, h - crop_height)
+        finish_w = start_w + crop_width
+        finish_h = start_h + crop_height
+        left_image_crop = left_image[start_h:finish_h, start_w:finish_w]
+        right_image_crop = right_image[start_h:finish_h, start_w:finish_w]
+        disp_image_crop = disp_image[start_h:finish_h, start_w:finish_w]
+        # return stereo pair and disp images
+        return [left_image_crop, right_image_crop, disp_image_crop]
+    elif kwargs.phase == 'train':
+        # crop images
+        h, w = left_image.shape[0:2]
+        start_w = random.randint(0, w - kwargs.preproc_args.crop_width)
+        start_h = random.randint(0, h - kwargs.preproc_args.crop_height)
+        finish_w = start_w + kwargs.preproc_args.crop_width
+        finish_h = start_h + kwargs.preproc_args.crop_height
+        left_image_crop = left_image[start_h:finish_h, start_w:finish_w]
+        right_image_crop = right_image[start_h:finish_h, start_w:finish_w]
+        disp_image_crop = disp_image[start_h:finish_h, start_w:finish_w]
+        # return stereo pair and disp images
+        return [left_image_crop, right_image_crop, disp_image_crop]
